ae3c965a9aed867431ed62f170fd661d3f5e8f27
kante714 <webnewbie14@gmail.com>
2024-08-29 15:23:12 +0545

    Second coding challenge and solution for today.

---
+++ CC 2024-07-29/Can_they_code_in_the_same_language.js
@@ -0,0 +1,61 @@
+// QUESTION //
+/*
+You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return either:
+
+true if all developers in the list code in the same language; or
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+your function should return true.
+
+Notes:
+
+The strings representing a given language will always be formatted in the same way (e.g. 'JavaScript' will always be formatted will upper-case 'J' and 'S'
+The input array will always be valid and formatted as in the example above.
+*/
+
+
+//PARAMETERS: an array of objects -> key and value can be of any datatypes
+
+//RETURNS: return true if all developers in the list code in the same language else return false
+
+//EXAMPLES: (Input --> Output)
+// let list1 = [
+//   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+// ];
+// list1 --> true
+
+//PSEUDOCODE:
+// create a function that take in an array of objects
+// take that array and loop over it 
+// check if language of every developers are same 
+// if language of every developers are same return true else return false 
+
+//SOLUTION:
+function isSameLanguage(list) {
+    return list.every(dev => dev.language === list[0].language)
+}
+
+let list1 = [
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+
+let list2 = [
+    { firstName: 'Mariami', lastName: 'G.', country: 'Georgia', continent: 'Europe', age: 29, language: 'Python' },
+    { firstName: 'Mia', lastName: 'H.', country: 'Germany', continent: 'Europe', age: 39, language: 'Ruby' },
+    { firstName: 'Maria', lastName: 'I.', country: 'Greece', continent: 'Europe', age: 32, language: 'C' },
+];
+
+console.log(isSameLanguage(list1), true);
+console.log(isSameLanguage(list2), false);
